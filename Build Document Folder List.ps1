b3383238d6c6a852250e17867418190eb3a85709
mjwork <mark.john@cgi.com>
2015-11-27 15:59:37 +1000

    Update Build Document Folder List.ps1

--- Build Document Folder List.ps1
+++ Build Document Folder List.ps1
@@ -1,16 +1,26 @@
-$session = New-PSSession -ComputerName BNESP
-Invoke-Command -Session $session -ScriptBlock { Add-PSSnapin Microsoft.SharePoint.PowerShell }
-Invoke-Command -Session $session -ScriptBlock {
-    $results = @()
-    Get-SPSite -WebApplication "http://bnesp" -limit All | ForEach-Object {  
-        foreach ($w in $_.AllWebs) {
-            foreach($docLib in $w.Lists | where {$_.BaseTemplate -eq "DocumentLibrary"}) {
-                $docLib.ParentWebUrl
-                
-                foreach ($folder in $docLib.Folders) {
-                    $folder.URL
-                }
-            }        
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+Start-SPAssignment -global
+
+$results = @()
+$inputPath = "C:\Temp\SharePointSitesList.csv"
+$outputPath = "C:\Temp\DocLibrariesStructure.csv"
+$sharepointSitesList = Get-Content $inputPath
+
+ForEach($siteUrl in $sharepointSitesList)
+{
+    ("Processing site: " + $siteUrl)
+    Get-SPWeb -Identity $siteUrl | ForEach-Object {
+
+        foreach($docLib in $_.Lists | where {$_.BaseTemplate -eq "DocumentLibrary"}) 
+        {
+            $results += ($docLib | Select-Object -Property @{Name="SiteUrl"; Expression = {$siteUrl}}, @{Name="Path"; Expression = {$_.RootFolder}})
+            $folders = $docLib.Folders
+            $results += ($folders | Select-Object -Property @{Name="SiteUrl"; Expression = {$siteUrl}}, @{Name="Path"; Expression = {$_.Url}})
         }
     }
 }
+
+$results | Export-Csv -Path $outputPath -NoTypeInformation
+
+Stop-SPAssignment -global
